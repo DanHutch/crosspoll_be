3fa206874861cbd1b3df3147a820f1e472618c6d
DanHutch <danhutch5280@gmail.com>
2019-02-13 16:58:33 -0700

    Implement GET /vendors endpoint through TDD

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,17 +1,33 @@
 require 'rails_helper'
 
 describe "Vendors API" do
-	it "sends a list of vendors" do
-		use_seeds
-    binding.pry
-		# get '/api/v1/merchants'
+  it "sends a list of vendors" do
+    use_seeds
 
-		# expect(response).to be_successful
+		get '/api/v1/vendors'
 
-		# merchants = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
 
-		# expect(merchants["data"].count).to eq(3)
+    vendors = JSON.parse(response.body)
 
+    expect(vendors["data"].count).to eq(10)
+
+    vendors["data"].each do |vendor|
+      expect(vendor["attributes"]).to have_key("name")
+      expect(vendor["attributes"]).to have_key("account_type")
+      expect(vendor["attributes"]).to have_key("address")
+      expect(vendor["attributes"]).to have_key("city")
+      expect(vendor["attributes"]).to have_key("state")
+      expect(vendor["attributes"]).to have_key("zip")
+      expect(vendor["attributes"]).to have_key("email")
+      expect(vendor["attributes"]).to have_key("phone")
+      expect(vendor["attributes"]).to have_key("lat")
+      expect(vendor["attributes"]).to have_key("long")
+      expect(vendor["attributes"]).to have_key("bio")
+      expect(vendor["attributes"]).to have_key("products")
+      expect(vendor["attributes"]).to have_key("img_url")
+    end
 	end
 
 	# it "can get a single merchant by id" do
