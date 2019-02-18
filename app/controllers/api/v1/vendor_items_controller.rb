4d5b199595a5b7bc18f30849ac768e6899ec98d0
DanHutch <danhutch5280@gmail.com>
2019-02-17 19:11:59 -0700

    Implement authenticated endpoint to update a vendor_item

--- app/controllers/api/v1/vendor_items_controller.rb
+++ app/controllers/api/v1/vendor_items_controller.rb
@@ -5,7 +5,16 @@ class Api::V1::VendorItemsController < ApplicationController
     attempt_save_vendor_item(current_user.vendor_items.new(vendor_item_params))
   end
 
+  def update
+
+    attempt_update_vendor_item(current_user.vendor_items.find(params[:vendor_item_id]))
+  end
+
 private
+
+  def update_vendor_item_params
+    params.permit(:price, :unit, :description)
+  end
 
   def vendor_item_params
     params.permit(:item_id, :price, :unit, :description)
@@ -13,6 +22,14 @@ private
 
   def attempt_save_vendor_item(vendor_item)
     if vendor_item.save
+      render json: VendorItemSerializer.new(vendor_item), status: 201
+    else
+      render json: {"Error": "Something went wrong!"}, status: 422
+    end
+  end
+
+    def attempt_update_vendor_item(vendor_item)
+    if vendor_item.update(update_vendor_item_params)
       render json: VendorItemSerializer.new(vendor_item), status: 201
     else
       render json: {"Error": "Something went wrong!"}, status: 422
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       post '/authentication', to: "authentication#create"
 
       post '/users/:id/vendor_items', to: "vendor_items#create"
+      put '/users/:user_id/vendor_items/:vendor_item_id/', to: "vendor_items#update"
     end
   end
 end
